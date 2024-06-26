6ea0dd746d012bd39700d3f61aedddabd209a4fd
KCCI115\gladious <nuguri5165@naver.com>
2024-06-26 14:59:48 +0900

    feat: 초기 프로젝트 세팅
    - Entity 생성 확인
    - Springboot 시작 확인

--- src/main/java/com/server/intranet/login/controller/LoginController.java
+++ src/main/java/com/server/intranet/login/controller/LoginController.java
@@ -1,11 +1,7 @@
 package com.server.intranet.login.controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class LoginController {
 
-    public static void main(String args[]){
-        System.out.println("test");
-    }
+public class LoginController {
 
 }
