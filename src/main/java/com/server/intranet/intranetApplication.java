7e0bdb9f6bfbe307f7d22350938687bfd2e0610e
MoonSHD <kevin45077@gmail.com>
2024-06-26 14:49:28 +0900

    application파일 추가

---
+++ src/main/java/com/server/intranet/intranetApplication.java
@@ -0,0 +1,12 @@
+package com.server.intranet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class intranetApplication {
+
+    public static void main(String args[]){
+        SpringApplication.run(intranetApplication.class, args);
+    }
+}
