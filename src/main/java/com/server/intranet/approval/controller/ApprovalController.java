295616593b775800df8bf915514a86afe1975b7e
KCCI115\gladious <nuguri5165@naver.com>
2024-06-28 09:38:24 +0900

    feat: 전자결재 등록 폼에서 불러올 양식 목록 조회 작업
    전자결재 등록 폼에서 가져올 양식 목록 조회 로직 작성 및 엔티티 추가

--- src/main/java/com/server/intranet/approval/controller/ApprovalController.java
+++ src/main/java/com/server/intranet/approval/controller/ApprovalController.java
@@ -1,4 +1,40 @@
 package com.server.intranet.approval.controller;
 
+import com.server.intranet.approval.dto.ApprovalFormResponseDTO;
+import com.server.intranet.approval.service.impl.ApprovalServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/app/approval")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class ApprovalController {
+
+    private final ApprovalServiceImpl approvalService;
+
+    @GetMapping("/form")
+    public ResponseEntity<Map<String, Object>> selectFormList() throws Exception{
+        List<ApprovalFormResponseDTO> formList = approvalService.selectFormList();
+        List<ApprovalFormResponseDTO> storageList = approvalService.selectStorageList();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("formList", formList);
+        data.put("storageList", storageList);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("data",data);
+        map.put("code", "SUCCESS");
+        map.put("msg", "조회가 완료되었습니다.");
+
+        return ResponseEntity.ok().body(map);
+    }
 }
--- src/main/java/com/server/intranet/approval/service/impl/ApprovalServiceImpl.java
+++ src/main/java/com/server/intranet/approval/service/impl/ApprovalServiceImpl.java
@@ -1,4 +1,85 @@
 package com.server.intranet.approval.service.impl;
 
-public class ApprovalServiceImpl {
+import com.server.intranet.approval.dto.ApprovalFormResponseDTO;
+import com.server.intranet.approval.entity.ApprovalForm;
+import com.server.intranet.approval.entity.Storage;
+import com.server.intranet.approval.repository.ApprovalFormRepository;
+import com.server.intranet.approval.repository.StorageRepository;
+import com.server.intranet.approval.service.ApprovalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * The type Approval service.
+ */
+@Service
+@RequiredArgsConstructor
+public class ApprovalServiceImpl implements ApprovalService {
+    //양식 폼 레포지토리
+    private final ApprovalFormRepository formRepository;
+    private final StorageRepository storageRepository;
+
+    /**
+     * methodName : selectFormList
+     * author : YunJae Lee
+     * description : 양식 폼 목록 불러오기
+     *
+     *
+     * @return list
+     * @throws Exception the exception
+     */
+    @Override
+    public List<ApprovalFormResponseDTO> selectFormList() throws Exception {
+        List<ApprovalForm> list = formRepository.findAll();
+        List<ApprovalFormResponseDTO> formList = new ArrayList<>();
+        for(ApprovalForm form : list){
+            if(!list.isEmpty()){
+                ApprovalFormResponseDTO responseDTO = ApprovalFormResponseDTO.builder()
+                        .formId(form.getFormId())
+                        .subject(form.getSubject())
+                        .content(form.getContent())
+                        .auth(form.getAuth())
+                        .preApproval(form.getPreApproval())
+                        .option(form.getOption())
+                        .status(form.getStatus())
+                        .storageId(form.getStorageId().getStorageId())
+                        .storageName(form.getStorageId().getName())
+                        .build();
+                formList.add(responseDTO);
+            }
+        }
+
+        return formList;
+    }
+
+
+    /**
+     * methodName : selectStorageList
+     * author : YunJae Lee
+     * description : 저장소 목록 조회
+     *
+     *
+     * @return list
+     * @throws Exception the exception
+     */
+    @Override
+    public List<ApprovalFormResponseDTO> selectStorageList() throws Exception {
+        List<Storage> list = storageRepository.findAll();
+        List<ApprovalFormResponseDTO> storageList = new ArrayList<>();
+        for(Storage storage : list){
+            if(!list.isEmpty()){
+                ApprovalFormResponseDTO responseDTO = ApprovalFormResponseDTO.builder()
+                        .storageId(storage.getStorageId())
+                        .storageName(storage.getName())
+                        .build();
+                storageList.add(responseDTO);
+            }
+        }
+
+        return storageList;
+    }
 }
