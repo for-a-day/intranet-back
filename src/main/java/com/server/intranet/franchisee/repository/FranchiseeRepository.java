e77aad74d9b114bc1d28cebb9f00e4008902975b
kyl <yello0755@naver.com>
2024-06-28 16:59:09 +0900

    가맹점 수정, 등록 기능 구현

--- src/main/java/com/server/intranet/franchisee/repository/FranchiseeRepository.java
+++ src/main/java/com/server/intranet/franchisee/repository/FranchiseeRepository.java
@@ -5,23 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.server.intranet.franchisee.entity.FranchiseeEntity;
 
+@Repository
 public interface FranchiseeRepository extends JpaRepository<FranchiseeEntity, String> {
 	// 가맹점 목록 화면
 	List<FranchiseeEntity> findAll();
 }
-
-
-//@Query("SELECT f " +
-//"FROM FranchiseeEntity f " +
-//"WHERE f.employee_id = :employee")
-//List<FranchiseeEntity> findFranchiseesByEmployee(@Param("employee") Long employee);
-
-//// 사원 아이디가 동일한 사람의 사원명 조회
-//@Query("SELECT e.NAME " +
-//"FROM FranchiseeEntity f " +
-//"JOIN f.employee_id e " +
-//"WHERE f.employee_id = :employee")
-//List<String> findEmployeeNamesByFranchiseeEmployeeId(@Param("employee") Long employee);
