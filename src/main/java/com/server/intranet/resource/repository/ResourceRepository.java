9d2266ab9bb44fda5294ad841d2df2c6237e98ee
MoonSHD <kevin45077@gmail.com>
2024-06-28 17:35:11 +0900

    인사관리 CRUD생성 (U미완성)

--- src/main/java/com/server/intranet/resource/repository/ResourceRepository.java
+++ src/main/java/com/server/intranet/resource/repository/ResourceRepository.java
@@ -1,6 +1,9 @@
 package com.server.intranet.resource.repository;
 
 
+import com.server.intranet.resource.dto.ResourceRequestDTO;
+import com.server.intranet.resource.entity.AuthorityEntity;
+import com.server.intranet.resource.entity.DepartmentEntity;
 import com.server.intranet.resource.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,3 +11,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ResourceRepository extends JpaRepository<EmployeeEntity, Long> {
 }
+
