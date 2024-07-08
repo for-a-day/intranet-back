add1dfa4942e7b67953b371e5d9c1dda8e350727
kyl <yello0755@naver.com>
2024-07-08 21:42:12 +0900

    WarningRequestDto 의존성 확정 (해당파일 수정하신 분은 수정사항 폐기 후 커밋부탁드려요)

--- src/main/java/com/server/intranet/warning/dto/WarningRequestDto.java
+++ src/main/java/com/server/intranet/warning/dto/WarningRequestDto.java
@@ -2,16 +2,8 @@ package com.server.intranet.warning.dto;
 
 import lombok.Getter;
 import lombok.NonNull; 
-
-import com.server.intranet.closing.entity.ClosingEntity;
-import com.server.intranet.franchisee.entity.FranchiseeEntity;
-
-import lombok.Getter;
-import lombok.NonNull;
-
 import jakarta.annotation.Nullable;
 import lombok.Setter;
-import org.springframework.lang.Nullable;
 
 @Getter
 @Setter
